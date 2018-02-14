73ce31f25e10d0f1ba6b70f1d0be8e0f3bce9d81
atsushi130 <atsushi_main@outlook.jp>
2018-02-14 23:12:14 +0900

    add: Entity

---
+++ APIKitSample/Data/Entities/Testy/TestyEntity.swift
@@ -0,0 +1,10 @@
+//
+//  UserEntity.swift
+//  APIKitSample
+//
+//  Created by Atsushi Miyake on 2018/02/14.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+struct TestyEntity: Decodable {
+}
