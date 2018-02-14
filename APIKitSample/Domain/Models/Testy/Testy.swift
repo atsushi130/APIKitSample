8a6305c2332c846964ed4f0fa218adf95b5cbb02
atsushi130 <atsushi_main@outlook.jp>
2018-02-14 23:13:58 +0900

    add: Model

---
+++ APIKitSample/Domain/Models/Testy/Testy.swift
@@ -0,0 +1,12 @@
+//
+//  Testy.swift
+//  APIKitSample
+//
+//  Created by Atsushi Miyake on 2018/02/14.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+struct Testy {
+    init(entity: TestyEntity) {
+    }
+}
