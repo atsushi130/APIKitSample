ae9928f078cbe404f6516c60f70460c7d9d2a74d
atsushi130 <atsushi_main@outlook.jp>
2018-02-14 23:14:28 +0900

    add: sample code

---
+++ APIKitSample/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  APIKitSample
+//
+//  Created by Atsushi Miyake on 2018/02/14.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        TestyRespository.shared.testy.subscribe(onNext: { testy in
+            print(testy)
+        }).disposed(by: self.disposeBag)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
